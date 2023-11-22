3d9c4122dcfa98a039b205cddd52980ba845fb43
Danna Marin <d.marin3397@gmail.com>
2023-11-22 16:22:01 -0500

    initial setup

---
+++ src/tests/base-pruebas.test.js
@@ -0,0 +1,10 @@
+test('This test should not fail', () => { 
+    // 1. Initialization
+    const message1 = 'Hola Mundo';
+
+    // 2. Stimulation
+    const message2 = 'Hola Mundo'
+
+    // 3. Expected behaviour
+    expect(message1).toBe(message2)
+ })
